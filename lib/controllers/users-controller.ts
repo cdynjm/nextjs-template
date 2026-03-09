022e61e4fd198d597d7f270283d8a82ea69d7db0
Jemuel Cadayona <cdynjm@gmail.com>
2026-03-09 09:46:15 +0800

    update

--- lib/controllers/users-controller.ts
+++ lib/controllers/users-controller.ts
@@ -1,18 +1,17 @@
 import { encrypt, generateKey } from "@/lib/security/cipher";
-import { users } from "../db/models";
-import { db } from "@/lib/db/connection";
+import { prisma } from '@/lib/db/prisma'
 
 export class UsersController {
   static async getUsers() {
     try {
       const key = await generateKey();
-      const usersData = await db.select().from(users);
+      const usersData = await prisma.user.findMany();
 
       return await Promise.all(
         usersData.map(async (user) => ({
           encrypted_id: await encrypt(user.id.toString(), key),
           email: user.email,
-          createdAt: user.createdAt,
+          createdAt: user.created_at,
         })),
       );
     } catch (error) {
